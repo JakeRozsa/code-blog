7f08c65eac484721d5641e47acc90a823b7a40ae
Jacob Rozsa <jakebrozsa@gmail.com>
2019-05-14 12:38:44 -0600

    logo & navbar

--- wwwroot/code-blog/src/components/Navbar.vue
+++ wwwroot/code-blog/src/components/Navbar.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="nav-bar">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand"  @click="$router.push({name:'home'})">LOGO</a>
+<nav class="navbar navbar-expand-xl navbar-light bg-light">
+    <div class="col-3">
+  <a class="navbar-brand"  @click="$router.push({name:'home'})"><img class="barlogo" src="@/assets/decodedlogopng.png" alt=""></a>
+    </div>
+    <div class="col-3 offset-3">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+    </div>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" @click="$router.push({name:'dictionary'})">Dictionary <span class="sr-only">(current)</span></a>
+        <a class="nav-link" @click="$router.push({name:'dictionary'})">
+            <p>Dictionary</p><span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link"  @click="$router.push({name:'challenges'})">Challenges</a>
+        <a class="nav-link"  @click="$router.push({name:'challenges'})">
+        <p>Challenges</p></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link"  @click="$router.push({name:'about'})">About</a>
+        <a class="nav-link"  @click="$router.push({name:'about'})">
+            <p>About</p></a>
       </li>
     </ul>
   </div>
@@ -38,5 +45,9 @@ export default {
 
 
 <style scoped>
-
+.barlogo{
+    cursor: pointer;
+    max-height: 75px;
+    max-width: 33vw;
+}
 </style>
